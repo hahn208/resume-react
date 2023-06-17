2d6aaa0cfa496c77b64484781c0e58f9a1f87019
208Hahn <208hahn@gmail.com>
2023-06-17 14:00:54 -0600

    Factored out data to JSON files and updated relevant code. Started on mobile-first styling.

--- src/App.tsx
+++ src/App.tsx
@@ -12,89 +12,22 @@ type ExperienceType = {
     stint: string,
     details: string,
     forceWidow: boolean
-}
+};
 
 /**
- * Array of objects typed by ExperienceType
+ * Import the work experience json.
  */
-const experienceSet: ExperienceType[] = [
-    {
-        'title' : 'Mattress Firm',
-        'logo' : 'mattressfirm.png',
-        'position' : 'Software Engineering Manager',
-        'stint' : 'May 2021 – Jun 2023 • Remote',
-        'details' : 'Engineering manager and tech-lead for multiple teams. Heavy lifted the re-platform from Salesforce to Microsoft D365 with individual contribution of React/SASS, diagramming multi-team dependencies, installing safety checks, and masterfully channeled torrent of customers from the legacy system to the new platform. Carrying teams over obstacles both technical, abstract, and personal. Sussed-out lurking issues quietly impacting millions in annual revenue. Marshaling tiger teams to triage and stem catastrophic revenue bleed. Forged the SDLC across multiple development teams and continue to pilot weekly launches. Orchestrating cross-team (CRM, ERP, BI, F&O) efforts for mission critical feature launches, perfecting alignment of impacted systems. Accelerating teams with code reviews, shared knowledge sessions, tools for enablement, and awful puns.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'Thrive Web Designs (agency)',
-        'logo' : 'thrive.png',
-        'position' : 'Sr. Full Stack Developer',
-        'stint' : 'Sep 2015 – May 2021 • Boise, Idaho',
-        'details' : 'Fastened close relationships with clients. Piloted sites given concept / design to fully deployed mobile-first, ADA compliant, and PageInsights "A" rated. With object oriented PHP, custom MySQL queries & stored procedures, SASS, class-based ES6 JS, and HTML5 the Wordpress platform was adapted and extended to enrich client capabilities. Exacted bugs on some of the worst greenfield projects imaginable. Instituted personal coding standards and disciplines. Mentored junior devs and new hires. Detailed setup and maintenance instructions for team collective knowledge capital, ie critical backup systems, massive data batch processing, and deployment playbooks. Highlighted work: PBS Kids, Ada County ID, IDVSA.org, Lee Pesky Learning Center, Syringa Properties, Boise State University.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'Mattress Firm',
-        'logo' : 'mattressfirm.png',
-        'position' : 'Software Engineer (contract)',
-        'stint' : 'Feb 2020 – Aug 2020 • Remote',
-        'details' : 'Originally assigned HTML and CSS support, but swiftly advanced to full stack developer. Tooled CMS modules, enabling the marketing and operations teams to manage their own promotions. This corrected the situation where skilled developers were under-utilized for trivial HTML tasks. In the short six-month contract: Immersed myself in the tech-stack, delivered expedient results while clearing the two-month user-story backlog, on-boarded two replacement hires.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'CenturyLink (now Lumen telecom)',
-        'logo' : 'centurylink.png',
-        'position' : 'Frontend Developer',
-        'stint' : 'Sep 2012 – Aug 2015 • Boise, Idaho',
-        'details' : 'Evolved codebase to eliminate dependencies on framework shims, decreasing severe technical debt and infrastructure fragility. Authored user-stories for Scrum team to address general performance and usability issues. Conducted A/B tests to measure boosts in revenue achieved by calibrating user experience elements. Engineered user funnel tracing to expose user dead-ends. Directed and delivered browser performance tuning to cull high bounce rate by enacting image optimization, dismantled render blocking, eliminated code redundancies, and shed dead code weight. At times achieved 10x page speed improvement. Empowered team with chrome extension and site tool to remove user interface snags on internal CMS and code repository program.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'ShoutStage (now Vinyl)',
-        'logo' : 'shoutstage.png',
-        'position' : 'Software Engineer',
-        'stint' : 'Mar 2012 – Aug 2012 • Boise, Idaho',
-        'details' : 'Contributed to a consolidated communication platform with chat API integrations. Drove technology decisions. Heavily researched browser limitations. Bleeding edge work in NodeJS 0.12 and MongoDB.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'UpTop Corp (agency)',
-        'logo' : 'uptop.png',
-        'position' : 'Software Engineer',
-        'stint' : 'Jul 2006 – Feb 2012 • Boise, Idaho',
-        'details' : 'Owned high traffic projects with CenturyLink telecom. Balanced rapid pace concurrent promos broadcast to hundreds of thousands of customers. Various local and international projects.',
-        'forceWidow': false
-    }
-];
+const experienceSet: ExperienceType[] = require('./data/experience.json');
 
-const volunteer: ExperienceType[] = [
-    {
-        'title' : 'American Red Cross',
-        'logo': 'redcross.png',
-        'position': 'Blood Donor',
-        'stint': '',
-        'details': 'Since 2019, I have given over 8.5 liters of blood.',
-        'forceWidow': false
-    },
-    {
-        'title' : 'GitHub',
-        'logo': 'github.png',
-        'position': 'Open Source Contributor',
-        'stint': '',
-        'details': 'Logged issues and contributed resolutions through pull requests.',
-        'forceWidow': false
-    }
-];
+/**
+ * Import the volunteer experience json.
+ */
+const volunteer: ExperienceType[] = require('./data/volunteer.json');
 
-const education: ExperienceType = {
-    'title' : 'College of Idaho',
-    'logo': 'cofi.png',
-    'position': 'Bachelors • Mathematics and Computer Science',
-    'stint': '2002 – 2006 • Caldwell, Idaho',
-    'details': 'Proposed and negotiated creation of new student engineer positions in IT department. Served campus community by daylighting student organizations with websites, spearheaded school’s first online voting system, and established two new computer labs.',
-    'forceWidow': false
-};
+/**
+ * Import the education experience json.
+ */
+const education: ExperienceType[] = require('./data/education.json');
 
 const skillSet: { [key: string]: string[] } = {
     'pro': ['HTML5', 'ES6', 'NodeJS', 'React', 'TypeScript', 'CSS3', 'SCSS', 'Jest', 'PHP7', 'MySQL', 'REST', 'oAuth', 'Linux', 'WordPress', 'WCAG'],
@@ -102,10 +35,9 @@ const skillSet: { [key: string]: string[] } = {
     'tools': ['Docker', 'Cloudways', 'Adobe DTM', 'GTM', 'Git', 'Yarn', 'JetBrains', 'Jira', 'Azure', 'Figma', 'Photoshop']
 };
 
-const bio: { [key: string]: string | object } = {
+const bio: { [key: string]: object } = {
     'title': <h1><em>Andrew</em> <em>Hahn</em></h1>,
     'intro': <p>Professional software engineer since 2004 • Traversed hundreds of miles of Idaho back-country • Designed and implemented home geothermal heating system • Cultivated and leavened <i>scores</i> of sourdough loaves • Sired the cutest / dorkiest child of the Hahn lineage.</p>,
-    'logo': '',
     'address': <p>Boise, Idaho<br/>208ha<span className={'no-spam'}>asdf</span>hn&#64;gmail&#46;com<br/>208-283-52<span className={'no-spam'}>4321</span>98</p>,
     'slogan': <p>Endeavor with our <strong>heads together</strong>.</p>
 };
@@ -137,15 +69,33 @@ function ResumeItem(props: { experience: ExperienceType; })
 }
 
 /**
- * ResumeMain
- * Display bio, iterate over the array of experience objects, output education.
+ * ResumeExperience
+ * Iterate over the arrays of experience objects.
  *
  * @return JSX
  */
-function ResumeMain()
+function ResumeExperience()
 {
     return(
         <div className={'grid-area-experience'}>
+            <>
+                <h2><em>Education</em></h2>
+                { education.map(experienceItem => (<ResumeItem experience={experienceItem}/>)) }
+                <h2><em>Experience</em></h2>
+                { experienceSet.map(experienceItem => (<ResumeItem experience={experienceItem}/>)) }
+            </>
+        </div>
+    );
+}
+
+/**
+ * 
+ * @return JSX
+ */
+function ResumeBio()
+{
+    return(
+        <div className={'grid-area-bio'}>
             <>
                 { bio.title }
                 <section>
@@ -153,14 +103,12 @@ function ResumeMain()
                         {bio.intro}<p>View Source: <a href="https://github.com/hahn208/resume-react" target="_blank" rel={"noreferrer"}>github.com/hahn208/resume-react</a></p>
                     </>
                 </section>
-                <h2><em>Education</em></h2>
-                <ResumeItem experience={education}/>
-                <h2><em>Experience</em></h2>
-                { experienceSet.map(experienceItem => (<ResumeItem experience={experienceItem}/>)) }
             </>
         </div>
-    );
+    )
 }
+
+
 
 /**
  * Sidebar
@@ -171,14 +119,14 @@ function ResumeMain()
 function Sidebar()
 {
     /**
-     * Build an array of list items containing skills
+     * Build an array of html list items containing skills.
      *
-     * @param set
+     * @param skillSet
      */
-    let skillsOutput = (set: Array<string>) => (
-        set.map(
-            (item: string, index: number) => {
-                return <li>{item}</li>;
+    let skillsOutput = (skillSet: Array<string>) => (
+        skillSet.map(
+            (skill: string) => {
+                return <li>{skill}</li>;
             }
         )
     );
@@ -211,14 +159,20 @@ function Sidebar()
     );
 }
 
-function App()
+/**
+ * The main app to display sidebar and content.
+ * 
+ * @constructor
+ */
+function App() 
 {
-  return (
-    <div className="App">
-      <Sidebar/>
-      <ResumeMain/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar/>
+            <ResumeBio/>
+            <ResumeExperience/>
+        </div>
+    );
 }
 
 export default App;
